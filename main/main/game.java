a90872bd5bbb6b60a865c7a34ac03905d7b90d21
HamailAzam <hamailazam@gmail.com>
2024-11-03 21:28:24 +0100

    Finished main game

--- main/main/game.java
+++ main/main/game.java
@@ -22,8 +22,43 @@ public class game {
 
         System.out.println("Let the game begin!");
 
+        do {
+            // player 1's turn
+            String Start1;
+            String Start2;
+            do {
+                System.out.println(player1.getPlayerName() + " start by writing Roll");
+                Start1 = scanner.nextLine();
+            } while (!Start1.equalsIgnoreCase("roll"));
 
-        
+            main.DiceResult diceResult1 = main.DiceRollMechanism.rollDice(); // Now we return DiceResult
+            int total1 = diceResult1.total; // Get total from DiceResult
+            System.out.println(player1.getPlayerName() + " rolled " + diceResult1.die1 + " and " + diceResult1.die2 + " a total of " + total1);
+            main.boardLogic BoardLogic = new main.boardLogic();
+            BoardLogic.boardLogicGame(total1, player1);
+            System.out.println(player1.getPlayerName() + " your balance is " + player1.getPlayerBalance());
 
+            do {
+                System.out.println(player2.getPlayerName() + " start by writing Roll");
+                Start2 = scanner.nextLine();
+            } while (!Start2.equalsIgnoreCase("roll"));
+
+            main.DiceResult diceResult2 = main.DiceRollMechanism.rollDice(); // Now we return DiceResult
+            int total2 = diceResult2.total; // Get total from DiceResult
+            System.out.println(player2.getPlayerName() + " rolled " + diceResult1.die1 + " and " + diceResult1.die2 + " a total of " + total2);
+            BoardLogic.boardLogicGame(total2, player2);
+            System.out.println(player2.getPlayerName() + " your balance is " + player2.getPlayerBalance());
+
+
+        } while (player1.getPlayerBalance() < 3000 && player2.getPlayerBalance() < 3000);
+
+        if (player1.getPlayerBalance() >= 3000) {
+            System.out.println(player1.getPlayerName() + " Has won");
+        } else if (player1.getPlayerBalance() == player2.getPlayerBalance()) {
+            System.out.println(player2.getPlayerName() + " Has won");
+        } else
+            System.out.println("The game is a draw");
     }
+
+
 }
