d5262429e9137ab5f698ba136e56564772e9716e
Alexander <alexanderkraibergb@gmail.com>
2024-11-03 17:07:57 +0100

    Working Spiller class

--- main/spiller.java
+++ main/spiller.java
@@ -1,6 +1,27 @@
+package main;
+
 public class spiller {
     private String playerName;
-    private konto playerKonto;  // Instance of konto
-}
+    private main.konto playerKonto;  // Instance of konto
 
-//didnt commit
+    public spiller() {
+        this.playerKonto = new main.konto();  // Initialize konto
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    // Delegate balance methods to konto
+    public int getPlayerBalance() {
+        return playerKonto.getBalance();
+    }
+
+    public void setPlayerBalance(int playerBalance) {
+        playerKonto.setBalance(playerBalance);
+    }
+} 
