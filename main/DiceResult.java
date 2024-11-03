3e20f484fadaed079c8ae262751b97a88e284fec
Raul Malik <37158219+RaulMalik@users.noreply.github.com>
2024-11-03 17:34:16 +0100

    imported dice function and storage from last game.

--- main/DiceResult.java
+++ main/DiceResult.java
@@ -1,3 +1,15 @@
+package main;
+
 public class DiceResult {
 
+    public int die1;
+    public int die2;
+    public int total;
+
+    public DiceResult(int die1, int die2, int total) {
+        this.die1 = die1;
+        this.die2 = die2;
+        this.total = total;
+    }
 }
+// imported from last game.
--- main/DiceRollMechanism.java
+++ main/DiceRollMechanism.java
@@ -1,3 +1,18 @@
+package main;
+
+import java.util.Random;
+
 public class DiceRollMechanism {
 
+        public static DiceResult rollDice() {
+
+            Random random = new Random();
+            int x = random.nextInt(6) + 1; // Roll die 1
+            int y = random.nextInt(6) + 1; // Roll die 1
+
+            int total = x + y;             // Calculate total
+
+            return new DiceResult(x, y, total); // Return all values in a DiceResult object
+        }
 }
+// imported from last game.
